c283f08afc85b12c69b7146cc08892e5f6c2bc41
Saurabh Tripathi <saurabh.umesh.tripathi@gmail.com>
2023-10-02 16:21:45 +0530

    Temporary commit

--- src/Solution.jsx
+++ src/Solution.jsx
@@ -4,19 +4,7 @@ export default function Solution() {
 
 
 
-    const panic = text => {
-
-        const lenthOfSplitArray = text.split(" ").length
-
-        let textModified = text.includes(' ') ?
-
-            text.split(' ').map((token, index) => lenthOfSplitArray - 1 !== index ? `${token.toUpperCase()} 😱` : `${token.toUpperCase()}!`)
-            :
-
-            `${text.toUpperCase()}!`
-
-        return lenthOfSplitArray > 1 ? textModified.join(' ') : textModified
-    }
+    const panic = text => text.split(' ').join(' 😱 ') + '!'
 
 
 
@@ -24,7 +12,7 @@ export default function Solution() {
 
     return (
         <h5>
-            {panic("special tea")}
+            {panic("I'm out of coffee")}
             <br/>
             {panic("bravo")}
         </h5>
