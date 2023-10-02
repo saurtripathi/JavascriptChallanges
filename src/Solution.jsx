349b88f71b40fcba098d192a5b0415eafd5a6860
Saurabh Tripathi <saurabh.umesh.tripathi@gmail.com>
2023-10-02 17:28:23 +0530

    Temporary commit

--- src/Solution.jsx
+++ src/Solution.jsx
@@ -2,17 +2,12 @@ import React from 'react'
 
 export default function Solution() {
 
-    // input: "The KITTENS are SLEEPING!"
-    // output: "shh... the kittens are sleeping"
-
-    const whisper = text => {
-        text = text.endsWith('!') ? text.slice(0, text.length - 1).toLowerCase() : text.toLowerCase()
-        return 'sh...'.concat(text)
-    }
-
+    const whisper = text => text.endsWith('!') ?
+        'sh... ' + text.toLowerCase().slice(0, text.indexOf('!')) :
+        'sh... ' + text.toLowerCase()
     return (
         <h5>
-            {whisper("MA'AM, this is a Wendy's!")}
+            {whisper("MA'AM, this is a Wendy's")}
         </h5>
     )
 }
