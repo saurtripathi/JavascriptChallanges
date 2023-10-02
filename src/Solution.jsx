f447771643da5187cb469d0dd506c799ba3019fa
Saurabh Tripathi <saurabh.umesh.tripathi@gmail.com>
2023-10-02 17:49:18 +0530

    Temporary commit

--- src/Solution.jsx
+++ src/Solution.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function Solution() {
 
-    const whisper = text => text.endsWith('!') ?
-        'sh... ' + text.toLowerCase().slice(0, text.indexOf('!')) :
-        'sh... ' + text.toLowerCase()
+    // "I'm so happy it's Monday" Example output: "I'M So hApPy iT'S MoNdAy"
+
+    const altCaps = text => text.split('').map((token, index) => index % 2 === 0 ? token.toUpperCase() : token).join('')
     return (
         <h5>
-            {whisper("MA'AM, this is a Wendy's")}
+            {altCaps("I'm so happy it's Monday")}
         </h5>
     )
 }
